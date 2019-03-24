9b7680995b5ebc34a9cdde1a8603dc98c8c081c6
Diogo Menezes <diogolmenezes@gmail.com>
2019-03-24 12:03:31 -0300

    finalizing database, redis and server

--- _sandbox/api/modules/sample-module/route.js
+++ _sandbox/api/modules/sample-module/route.js
@@ -1,5 +1,10 @@
-const { route } = require('simple-node-framework');
-
+const server = require('../../../index.js');
+const { route, redis } = require('simple-node-framework');
 const { info }  = route.info(__filename);
 
-
+server.get('/teste', (req, res, next) => {
+    res.send('Olaaa');
+    redis.set('MINHA', 'VALOR');
+    redis.delPattern('*')
+    return next();
+});
