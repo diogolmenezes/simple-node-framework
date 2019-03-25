a79acf05e6b52522bcc93cce042578656ef34447
Diogo Menezes <diogolmenezes@gmail.com>
2019-03-24 23:12:00 -0300

    eslint

--- _sandbox/api/modules/sample-module/controller.js
+++ _sandbox/api/modules/sample-module/controller.js
@@ -1,18 +1,17 @@
 const { BaseController } = require('simple-node-framework').Base;
-const { authorization } = require('simple-node-framework');
 
-class Controller extends BaseController {    
+class Controller extends BaseController {
     constructor() {
         super({
             module: 'My Sample Controller'
-        });        
+        });
     }
 
     get(req, res, next) {
-        const jwt = authorization.createJWT('nothing',  false);
+        super.activateRequestLog(req);
         this.log.debug('This is a sample log');
         res.send(200, 'Sample controller test');
-        req.cache.saveResponse(200, 'Sample controller test', res.headers, req)
+        req.cache.saveResponse(200, 'Sample controller test', res.headers, req);
         return next();
     }
 }
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const authorization = require('./lib/authorization');
 const config = require('./lib/config');
-const configFramework = require('./config/config-framework');
 const applicationError = require('./lib/errors');
 const logger = require('./lib/log');
 const redis = require('./lib/redis');
@@ -12,14 +11,12 @@ const Cache = require('./lib/cache');
 const database = require('./lib/database');
 const TestHelper = require('./lib/test');
 const Loggable = require('./lib/base/loggable');
-const RecaptchaMiddleware = require('./lib/middlewares/recaptchaMiddleware');
 const BaseClass = require('./lib/base/base');
 const BaseController = require('./lib/base/base-controller');
 const BaseService = require('./lib/base/base-service');
 const BaseRepository = require('./lib/base/base-repository');
 const BaseRest = require('./lib/base/base-rest');
 const BaseSoap = require('./lib/base/base-soap');
-const tokenizer = require('./lib/tokenizer');
 const Helper = require('./lib/util/helpers');
 const ProcessTimer = require('./lib/util/process-timer');
 const ControllerFactory = require('./lib/util/controller-factory');
@@ -31,13 +28,12 @@ module.exports = {
         BaseRepository,
         BaseRest,
         BaseService,
-        BaseSoap,
+        BaseSoap
     },
     ControllerFactory,
     database,
     Helper,
     Loggable,
-    RecaptchaMiddleware,
     ProcessTimer,
     Server,
     SessionHandler,
@@ -46,11 +42,9 @@ module.exports = {
     authorization,
     Cache,
     config,
-    configFramework,
     logger,
     redis,
     restifyErrors: applicationError.restifyErrors,
     route,
-    session,
-    tokenizer
+    session
 };
